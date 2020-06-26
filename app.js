1fc6aa4c4026f0b5036cce6248030260a8428180
Software/Nation <storiaca@gmail.com>
2020-06-26 16:46:31 +0200

    Create js for page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.set(express.static("public"));
+app.use(express.static("public"));
 
 app.use("/random-name", (req, res) => {
   const { first_name } = data[Math.round(Math.random() * data.length)];
@@ -18,4 +18,4 @@ app.use("/", (req, res) => {
   return res.render("index");
 });
 
-app.listen(3344, () => console.log("App listenin.."));
+app.listen(3344, () => console.log("App listening..."));
