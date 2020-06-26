18cd52986025573c1d554be228409e0ecdb74aee
Software/Nation <storiaca@gmail.com>
2020-06-26 16:54:57 +0200

    Add bootstrap

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ const loadRandomName = (resultDiv) => {
   fetch("http://localhost:3344/random-name")
     .then((response) => response.json())
     .then((result) => {
-      resultDiv.innerHtml = `<h2>${result.first_name} ${result.last_name}</h2>`;
+      resultDiv.classList.add("alert", "alert-success");
+      resultDiv.innerHTML = `<h2>${result.first_name} ${result.last_name}</h2>`;
     });
 };
